15173cecc27be6f39858de3687405af55c5c24ec
tae ryun Kim <taryun93@gmail.com>
2023-11-20 08:23:00 +0900

    chapter 10

---
+++ src/test/java/org/zerock/service/BoardServiceTests.java
@@ -0,0 +1,61 @@
+package org.zerock.service;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class BoardServiceTests {
+	
+@Setter(onMethod=@__({@Autowired}))
+private BoardService service;
+
+@Test
+public void testExist() {
+	log.info(service);
+	assertNotNull(service);
+}
+
+@Test
+public void testRegister() {
+	BoardVO board = new BoardVO();
+	board.setTitle("새로 작성하는 글");
+	board.setContent("새로 작성하는 내용");
+	board.setWriter("newbie");
+	
+	service.register(board);
+	log.info("생성된 게시물의 번호: " + board.getBno());
+}
+
+@Test
+public void testGetList() {
+//	람다식 service.getList()로부터 반환된 리스트의 항목을 foreach를 통해 반복하면서 log.info로 출력
+	service.getList().forEach(board -> log.info(board));
+}
+
+@Test
+public void testGet() {
+	log.info(service.get(1L));
+}
+
+@Test
+public void testUpdate() {
+	BoardVO board = service.get(1L);
+	if(board == null) {
+		return;
+	}
+	board.setTitle("제목 수정합니다.");
+	log.info("Modify Result: " + service.modify(board));
+}
+
+}

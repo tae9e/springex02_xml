ed13cd51a9cdda87d68c2433007d4cbb9db2d18d
hi <hi@DESKTOP-F4BC13L>
2023-11-20 18:13:24 +0900

    BoardController

---
+++ src/main/java/org/zerock/controller/BoardController.java
@@ -0,0 +1,22 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.service.BoardService;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/board/*")
+public class BoardController {
+	private BoardService service;
+	
+	@GetMapping("/list")
+	public void list(Model model) {
+		log.info("list");
+		model.addAttribute("list",service.getList());
+	}
+}
